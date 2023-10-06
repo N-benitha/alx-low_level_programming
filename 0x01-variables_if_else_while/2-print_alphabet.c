f59dc6fb52c576b3d32c26e9a64057093c428948
N-benitha <ngungabn03@gmail.com>
2023-10-06 13:29:11 -0700

    program that displays alphabets in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,17 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * print_alphabet - prints alphabets in lowercase
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i)
-		_putchar(i);
+		putchar(i);
 
-	_putchar('\n');
+	putchar('\n');
+
+	return (0);
 }
