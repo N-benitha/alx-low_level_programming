9531d0a877071f1f87cd57584e58b72eccee3334
N-benitha <ngungabn03@gmail.com>
2023-10-10 18:38:02 -0700

    last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,10 @@ int print_last_digit(int n)
 	int last_digit = n % 10;
 
 	if (n < 0)
-		return (lastdigit * -1);
+		return (last_digit * -1);
 
 	_putchar(last_digit + '0');
 
-	return (last_digit);
+	return (0);
 
 }
