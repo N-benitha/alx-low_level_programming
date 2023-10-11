36225a4f0b0ff0973abb7f2ee0e2a9c3028b0dd2
N-benitha <ngungabn03@gmail.com>
2023-10-10 19:52:26 -0700

    prime numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prime numbers
+ * @n: counter start
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 97; i --)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i < 99; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
