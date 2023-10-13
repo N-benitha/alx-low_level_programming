762af20808537ed00375429ed18d05c0b3799b0b
N-benitha <ngungabn03@gmail.com>
2023-10-12 18:30:12 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,29 +6,27 @@
  */
 int main(void)
 {
-	int i, a, b;
+	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i < 10)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			if (i % 3 == 0 && i % 5 != 0)
-			{
-				printf("Fizz");
-			}
-			else if (i % 5 == 0 && i % 3 != 0)
-			{
-				printf("Buzz");
-			}
-			else if (i % 3 == 0 && i % 5 == 0)
-			{
-				printf("FizzBuzz");
-			}
-			else
-			{
-				printf("%d", i);
-			}
+			printf("Fizz");
 		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
 		if (i != 100)
 			putchar(' ');
 		else
