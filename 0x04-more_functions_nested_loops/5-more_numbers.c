c8c13a4e2911e6ae914c13c125fb102b1193a3a3
N-benitha <ngungabn03@gmail.com>
2023-10-12 17:13:20 -0700

    10 times from 0 t0 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,29 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, res1, res2;
 
 	j = 0;
 
 	while (j < 10)
 	{
 		for (i = 0; i <= 14; i++)
-			_putchar(i + '0');
-		_putchar('\n');
+		{
+			if (i < 10)
+			{
+				res2 = i;
+			}
+			else
+			{
+				res1 = i / 10;
+				res2 = i % 10;
+				_putchar(res1 + '0');
+			}
+			_putchar(res2 + '0');
+		}
 		j++;
+
+		i = 0;
 	}
 	_putchar('\n');
 }
