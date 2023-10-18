369d26467d5be35613b11bbe9995074f8554bff2
N-benitha <ngungabn03@gmail.com>
2023-10-18 09:13:20 -0700

    work on it

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: string 1
+ * @src: string 2
+ * @n: number of bytes
+ *
+ * Return: pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, k = 0;
+
+}
