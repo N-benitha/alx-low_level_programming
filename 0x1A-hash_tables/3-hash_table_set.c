409652631a8992feeeb310987305e6eee774eba8
N-benitha <ngungabn03@gmail.com>
2024-04-27 05:59:29 -0700

    changes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,2 +1,51 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key to be added to the hash table
+ * @value: value of the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i;
+	hash_node_t *hn;
+	char *value_cp;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	value_cp = strdup(value);
+	if (value_cp == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_cp;
+			return (1);
+		}
+	}
+
+	hn = malloc(sizeof(hash_node_t));
+	if (hn == NULL)
+	{
+		free(value_cp);
+		return (NULL);
+	}
+	hn->key = strdup(key);
+	if (hn->key == NULL)
+	{
+		free(hn);
+		return (0);
+	}
+	hn->value = value_cp;
+	hn->next = ht->array[index];
+	ht->array[index] = hn;
+
+	return (1);
+}
