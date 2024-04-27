24d31c1cb151b3daccec55a6c5794323458e4259
N-benitha <ngungabn03@gmail.com>
2024-04-27 16:29:43 -0700

    changes

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,19 +11,19 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index, i;
-	char *value;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size || ht->array[index] == NULL)
+		return (NULL);
 
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			value = ht->array[i]->value;
-			return (value);
+			return (ht->array[i]->value);
 		}
 	}
 
