8c212533cdb8d753dabe79a91e2cae3e43293565
N-benitha <ngungabn03@gmail.com>
2024-04-28 05:57:57 -0700

    changes

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: pointer to the hash table to print
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	unsigned char comma_flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				if (comma_flag == 1)
+					printf(", ");
+				printf("\'%s\' : \'%s\'", node->key, node->value);
+				comma_flag = 1;
+				node = node->next;
+			}
+		}
+	}
+	printf("}\n");
+}
