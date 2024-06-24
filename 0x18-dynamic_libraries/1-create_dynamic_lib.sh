efc0861087de1aee08ae075307ccbe2318bc529d
N-benitha <ngungabn03@gmail.com>
2024-06-24 07:17:02 -0700

    changes

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 gcc TEST/*.c -c -fPIC
-gcc -shared -o liball.so.x.y -Wl,-soname,liball.so.x TEST/*.o
-ln -s liball.so.x.y liball.so.x
-ln -s liball.so.x liball.so
+gcc -shared -o liball.so -Wl,-soname,liball.so TEST/*.o
