d870020b496288cd0b4f85c9ba71a909da3cc293
N-benitha <ngungabn03@gmail.com>
2024-06-24 07:51:19 -0700

    updates

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcc -Wall -Wextra -Werror -pedantic -c -fPIC TEST/*.c
-gcc -shared -o liball.so -Wl,-soname,liball.so *.o
+gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
