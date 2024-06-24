c3a66d6eff5614484ccb1c2e4204761b65147d71
N-benitha <ngungabn03@gmail.com>
2024-06-24 16:10:18 -0700

    changes

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC TEST/*.c
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC abs.c isdigit.c memset.c strcat.c strcpy.c strncpy.c strstr.c atoi.c islower.c _putchar.c strchr.c strlen.c strpbrk.c isalpha.c isupper.c memcpy.c puts.c strcmp.c strncat.c strspn.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
